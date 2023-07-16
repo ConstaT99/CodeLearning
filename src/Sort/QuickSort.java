f491d7f407db354e3398fc79d1f32415e7a5997c
rtao6 <rtao6@illinois.edu>
2023-07-15 20:19:14 -0500

    Quick Sort Implemented

---
+++ src/Sort/QuickSort.java
@@ -0,0 +1,43 @@
+package Sort;
+
+public class QuickSort {
+    public static void quickSort(int[] nums, int left, int right){
+        if(left < right){
+            int pivot = partition(nums, left, right);
+            quickSort(nums,left,pivot - 1);
+            quickSort(nums,pivot+ 1, right);
+        }
+    }
+    // partition function to find the pivot and swap the value.
+    public static int partition(int[] nums, int left, int right){
+        int pivotIndex = left;
+        int pivot = nums[pivotIndex];
+        int l = left + 1;
+        int r = right;
+        while(l <= r){
+            if(nums[l] > pivot && nums[r] < pivot){
+                swap(nums,l++,r--);
+            }
+            if(nums[l] <= pivot) l++;
+            if(nums[r] >= pivot) r--;
+        }
+        swap(nums,pivotIndex,r);
+        return r;
+    }
+
+    public static void swap(int[]nums, int l , int r){
+        int temp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = temp;
+    }
+
+    public static void main(String[] args) {
+        Example examples = new Example();
+        int[] nums = examples.nums;
+        quickSort(nums,0,nums.length - 1);
+        for(int num : nums){
+            System.out.println(num);
+        }
+
+    }
+}
